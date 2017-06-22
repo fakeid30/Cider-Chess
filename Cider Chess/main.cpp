39a42149361752f4aab2814239b3a86b41e3928f
Fake ID <junayeed@protonmail.com>
2017-06-22 15:53:22 +0600

    Fixed position
    Trying to fix the position of the chess pieces

--- Cider Chess/main.cpp
+++ Cider Chess/main.cpp
@@ -14,21 +14,28 @@ int board[8][8] =
 	0, 0,  0,  0,   0,   0,  0, 0,
 	0, 0,  0,  0,   0,   0,  0, 0,
 	0, 0,  0,  0,   0,   0,  0, 0,
-	6, 6, 6, 6, 6, 6, 6, 6,
-	1, 2, 3, 4, 5, 3, 2, 1
-};
+	6, 6,  6,  6,   6,   6,  6, 6,
+	1, 2,  3,  4,   5,   3,  2, 1 };
+
+std::string toChessNote(Vector2f p)
+{
+	std::string s = "";
+	s += char(p.x / size + 97);
+	s += char(7 - p.y / size + 49);
+	return s;
+}
 
 void loadPosition()
 {
 	// should use int
-	auto k = 0;
-	for (auto i = 0; i < 8; i++)
-		for (auto j = 0; j < 8; j++)
+	int k = 0;
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
 		{
-			auto n = board[i][j];
+			int n = board[i][j];
 			if (!n) continue;
-			auto x = abs(n) - 1;
-			auto y = n > 0 ? 1 : 0;
+			int x = abs(n) - 1;
+			int y = n > 0 ? 1 : 0;
 			f[k].setTextureRect(IntRect(size*x, size*y, size, size));
 			f[k].setPosition(size*j, size*i);
 			k++;
@@ -46,19 +53,18 @@ int main()
 	Sprite s(t1);
 	Sprite sBoard(t2);
 
-	// should use int
-	for (auto i = 0; i < 32; i++) f[i].setTexture(t1);
+	for (int i = 0; i < 32; i++) f[i].setTexture(t1);
 
 	loadPosition();
 
-	// should use bool
-	auto isMove = false;
+	bool isMove = false;
 	float dx = 0, dy = 0;
+
+	int n = 0;
 
 	while (window.isOpen())
 	{
-		//can use Vector 2i
-		auto pos = Mouse::getPosition(window);
+		Vector2i pos = Mouse::getPosition(window);
 
 		Event e;
 		while (window.pollEvent(e))
@@ -69,22 +75,29 @@ int main()
 			// drag and drop
 			if (e.type == Event::MouseButtonPressed)
 				if (e.key.code == Mouse::Left)
-					if (s.getGlobalBounds().contains(pos.x, pos.y))
-					{
-						isMove = true;
-						dx = pos.x - s.getPosition().x;
-						dy = pos.y - s.getPosition().y;
-					}
+					for (int i = 0; i < 32; i++)
+						if (s.getGlobalBounds().contains(pos.x, pos.y))
+						{
+							isMove = true; n = i;
+
+							dx = pos.x - f[i].getPosition().x;
+							dy = pos.y - f[i].getPosition().y;
+						}
+
 			if (e.type == Event::MouseButtonReleased)
 				if (e.key.code == Mouse::Left)
+				{
 					isMove = false;
+					Vector2f p = f[n].getPosition() + Vector2f(size / 2, size / 2);
+					Vector2f newPos = Vector2f(size*int(p.x / size), size*int(p.y / size));
+					f[n].setPosition(newPos);
+				}
 		}
-		if (isMove) s.setPosition(pos.x - dx, pos.y - dy);
+		if (isMove) f[n].setPosition(pos.x - dx, pos.y - dy);
 		/// draw ///
 		window.clear();
 		window.draw(sBoard);
-		// use int
-		for (auto i = 0; i < 32; i++) window.draw(f[i]);
+		for (int i = 0; i < 32; i++) window.draw(f[i]);
 		window.display();
 	}
 }
